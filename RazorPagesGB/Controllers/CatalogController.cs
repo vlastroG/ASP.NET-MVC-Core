34a280b2ed38ade8211940ac44db4653ff11d066
Vladimir <vlastroganov@gmail.com>
2022-06-04 14:48:40 +0300

    Создать контроллер для каталога

---
+++ RazorPagesGB/Controllers/CatalogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPagesGB.Models;
+
+namespace RazorPagesGB.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static Catalog _catalog = new();
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        [HttpPost]
+        public IActionResult Products(Product product)
+        {
+            _catalog.Products.Add(product);
+            return View(_catalog);
+        }
+
+
+        [HttpGet]
+        public IActionResult ProductsAdd()
+        {
+            return View();
+        }
+    }
+}
