6ce95a00941915e54b0fad2cca3565c0377438fe
Vladimir <vlastroganov@gmail.com>
2022-06-04 11:11:57 +0300

    Add project files.

---
+++ TestBenchmark/Test.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBenchmark
+{
+    [MemoryDiagnoser]
+    public class Test
+    {
+        [Benchmark(Baseline = true)]
+        public void TestFirst()
+        {
+
+        }
+
+        [Benchmark]
+        public void TestSecond()
+        {
+
+        }
+    }
+}
