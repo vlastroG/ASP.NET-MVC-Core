d087a4644427afa8fd70287c44ae8d56264420a9
Vladimir <vlastroganov@gmail.com>
2022-06-05 14:59:04 +0300

    Сделать Catalog потокобезопасным

--- RazorPagesGB/Models/Catalog.cs
+++ RazorPagesGB/Models/Catalog.cs
@@ -2,6 +2,40 @@
 {
     public class Catalog
     {
-        public List<Product> Products { get; set; } = new();
+        private List<Product> _products { get; set; } = new();
+        private readonly object _syncProducts = new();
+        public void ProductAdd(Product product)
+        {
+            lock (_syncProducts)
+            {
+                _products.Add(product);
+            }
+        }
+
+        public void ProductDelete(int id)
+        {
+            lock (_syncProducts)
+            {
+                if (_products.FirstOrDefault(p => p.Id == id) != null)
+                {
+                    var product = _products.FirstOrDefault(p => p.Id == id);
+                    _products.Remove(product);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_syncProducts)
+            {
+                _products.Clear();
+            }
+        }
+
+        public IReadOnlyList<Product> ProductsGetAll()
+        {
+            var products_readonly = _products as IReadOnlyList<Product>;
+            return products_readonly;
+        }
     }
 }
