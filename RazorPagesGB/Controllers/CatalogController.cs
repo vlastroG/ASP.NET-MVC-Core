536038a62ff5c62272e4156d99b1c67f7d4427f9
Vladimir Stroganov <vlastroganov@gmail.com>
2022-06-23 00:59:35 +0300

    Добавить отправку email в контроллер

--- RazorPagesGB/Controllers/CatalogController.cs
+++ RazorPagesGB/Controllers/CatalogController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RazorPagesGB.Models;
+using RazorPagesGB.Services.EmailService;
 
 namespace RazorPagesGB.Controllers
 {
     public class CatalogController : Controller
     {
         private static Catalog _catalog = new();
+
+        private readonly IEmailService _emailService;
+
+        private readonly string _to = "savion.paucek81@ethereal.email";
+
+        public CatalogController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
 
         [HttpGet]
         public IActionResult Products()
@@ -17,18 +27,50 @@ namespace RazorPagesGB.Controllers
         public IActionResult Products(Product product)
         {
             _catalog.ProductAdd(product);
+            // добавить отправку email
+            string body =
+                "<h1>В каталог добавлен следующий товар:</h1>" +
+                "<tr>" +
+                    "<td>" + product.Id + " | </td>" +
+                    "<td>" + product.Name + " | </td>" +
+                    "<td>" + product.Description + " | </td>" +
+                    "<td>" + product.Price + " | </td>" +
+                    "<td><img src = \"" + @product.Image + "\" alt = \"Product photo.\"></td>" +
+                "</tr>";
+            _emailService.Send(new EmailDto()
+            {
+                Body = body,
+                Subject = "Добавлен товар",
+                To = _to
+            });
             return RedirectToAction("Products");
         }
 
         [HttpPost, ActionName("ProductDelete")]
         public IActionResult ProductDeleteConfirmed(int id)
         {
-            if (_catalog.ProductsGetAll()
-                .FirstOrDefault(p => p.Id == id) == null)
+            Product? product = _catalog.ProductsGetAll().FirstOrDefault(p => p.Id == id);
+            if (product == null)
             {
                 return NotFound();
             }
             _catalog.ProductDelete(id);
+            // добавить отправку email
+            string body =
+                "<h1>Из каталога удален следующий товар:</h1>" +
+                "<tr>" +
+                    "<td>" + product.Id + " | </td>" +
+                    "<td>" + product.Name + " | </td>" +
+                    "<td>" + product.Description + " | </td>" +
+                    "<td>" + product.Price + " | </td>" +
+                    "<td><img src = \"" + @product.Image + "\" alt = \"Product photo.\"></td>" +
+                "</tr>";
+            _emailService.Send(new EmailDto()
+            {
+                Body = body,
+                Subject = "Удален товар",
+                To = _to
+            });
             return RedirectToAction("Products");
         }
 
