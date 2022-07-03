040cd5468efbb185f3fce86720c96db9dd68d703
Vladimir Stroganov <vlastroganov@gmail.com>
2022-07-03 14:13:18 +0300

    Подключить Serilog

--- RazorPagesGB/Program.cs
+++ RazorPagesGB/Program.cs
@@ -1,37 +1,66 @@
 using RazorPagesGB.Configs;
 using RazorPagesGB.Services.EmailService;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.Configure<SmtpConfig>(builder.Configuration.GetSection("SmtpConfig"));
-
-// Время жизни - scope, т.к. на каждый запрос на добавление товара
-// и, следовательно, на запрос по отправке email уведомления об этом,
-// нужно создавать только один экземпляр класса сервиса отправки сообщений.
-// За один запрос может добавиться максимум 1 товар и вместе с ним 1 сообщение.
-builder.Services.AddScoped<IEmailService, EmailService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, conf) =>
+    {
+        conf
+        .MinimumLevel.Debug()
+        .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+        .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+        .ReadFrom.Configuration(ctx.Configuration);
+    });
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.Configure<SmtpConfig>(builder.Configuration.GetSection("SmtpConfig"));
+
+    // Время жизни - scope, т.к. на каждый запрос на добавление товара
+    // и, следовательно, на запрос по отправке email уведомления об этом,
+    // нужно создавать только один экземпляр класса сервиса отправки сообщений.
+    // За один запрос может добавиться максимум 1 товар и вместе с ним 1 сообщение.
+    builder.Services.AddScoped<IEmailService, EmailService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Server crashed!");
+}
+finally
+{
+    Log.Information("Shut doen complete.");
+    Log.CloseAndFlush();
+}
