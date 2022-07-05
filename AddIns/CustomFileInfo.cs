1c86975576412f76b0295a3f0e896a56d9151a74
Vladimir Stroganov <vlastroganov@gmail.com>
2022-07-05 21:48:47 +0300

    Заготовка для асинхронного чтения файлов

---
+++ AddIns/CustomFileInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddIns
+{
+    public static class CustomFileInfo
+    {
+        /// <summary>
+        /// Проверяет, является ли строка путем к файлу.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private static bool ValidateFilePath(string? path)
+        {
+            if (path is null)
+            {
+                return false;
+            }
+            FileInfo fileInfo = new FileInfo(path);
+            if (!fileInfo.Exists)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Принимает массив строк, являющихся путями к файлам, и считывает все строки из них.
+        /// Полученные строки записывает в новый файл.
+        /// </summary>
+        /// <param name="PathArray"></param>
+        /// <returns></returns>
+        public async static Task<string> GetFilesLinesAsync(string[] PathArray)
+        {
+
+        }
+    }
+}
