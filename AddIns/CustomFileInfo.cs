454f6922ce8253769f66e4abfe8c87f6848413f6
Vladimir Stroganov <vlastroganov@gmail.com>
2022-07-06 23:43:57 +0300

    Асинхронное чтение нескольких файлов

--- AddIns/CustomFileInfo.cs
+++ AddIns/CustomFileInfo.cs
@@ -1,25 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AddIns
+﻿namespace AddIns
 {
     public static class CustomFileInfo
     {
         /// <summary>
+        /// Читает файл асинхронно построчно и возвращает перечисление строк этого файла.
+        /// </summary>
+        /// <param name="path">Путь к файлу.</param>
+        /// <returns>Пуречисление всех строк файла.</returns>
+        private static async Task<IEnumerable<string>> ReadCharacters(string @path)
+        {
+            List<string> allLines = new List<string>();
+
+            using (StreamReader reader = new StreamReader(@path))
+            {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    allLines.Add(line);
+                }
+            }
+            return allLines as IEnumerable<string>;
+        }
+
+
+        /// <summary>
         /// Проверяет, является ли строка путем к файлу.
         /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        private static bool ValidateFilePath(string? path)
+        /// <param name="path">Строка.</param>
+        /// <returns>Является ли строка путем к файлу: true/false.</returns>
+        private static bool ValidateFilePath(string? @path)
         {
-            if (path is null)
+            if (String.IsNullOrEmpty(path))
             {
                 return false;
             }
-            FileInfo fileInfo = new FileInfo(path);
+            FileInfo fileInfo = new FileInfo(@path);
             if (!fileInfo.Exists)
             {
                 return false;
@@ -28,14 +43,45 @@ namespace AddIns
         }
 
         /// <summary>
+        /// Проверяет поданное перечисление строк на наличие корректных путей к файлам и возвращает их.
+        /// </summary>
+        /// <param name="stringPathsArray">Перечисление строк - путей к файлам.</param>
+        /// <returns>Перечисление строковых путей к файлам.</returns>
+        private static IEnumerable<string> ValidateFilePaths(IEnumerable<string> stringPathsArray)
+        {
+            List<string> filePaths = new();
+
+            foreach (var path in stringPathsArray)
+            {
+                if (ValidateFilePath(path))
+                {
+                    filePaths.Add(path);
+                }
+            }
+            return filePaths;
+        }
+
+        /// <summary>
         /// Принимает массив строк, являющихся путями к файлам, и считывает все строки из них.
         /// Полученные строки записывает в новый файл.
         /// </summary>
-        /// <param name="PathArray"></param>
-        /// <returns></returns>
-        public async static Task<string> GetFilesLinesAsync(string[] PathArray)
+        /// <param name="PathArray">Массив строк - путей к файлам.</param>
+        /// <returns>Перечисление всех строк всех файлов.</returns>
+        public async static Task<IEnumerable<string>> GetFilesLinesAsync(params string[] pathsArray)
         {
+            string[] filePaths = ValidateFilePaths(pathsArray).ToArray();
+            List<string> allLinesOfAllFiles = new();
 
+            foreach (string path in filePaths)
+            {
+                List<string>? allLinesOfFile = new();
+                allLinesOfFile = await ReadCharacters(path) as List<string>;
+
+#pragma warning disable CS8604 // Possible null reference argument.
+                allLinesOfAllFiles.AddRange(allLinesOfFile);
+#pragma warning restore CS8604 // Possible null reference argument.
+            }
+            return allLinesOfAllFiles;
         }
     }
 }
